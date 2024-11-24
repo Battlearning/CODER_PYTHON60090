63fae02e71cdfbe485a629dccffcfa0620f8c2a4
Jessica Battistino <batt.learning@gmail.com>
2024-11-24 12:30:10 -0300

    implemente funcion sumar

---
+++ ejemplo1.py
@@ -0,0 +1,10 @@
+def saludando():
+    print ("hola chicos estoy trabajando con git")
+
+    saludando()
+
+def sumar(n1,n2):
+    print ("el resultado de la suma entre n1 + n2 es de: ", n1 + n2)
+
+    sumar(int(input("ingresa el primer numero: ")),int(input("ingresa el segundo numero: ")))
+    
