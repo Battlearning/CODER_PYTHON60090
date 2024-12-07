0f3c8d6952fab3d2970c75455228caf5ee18c238
Jessica Battistino <batt.learning@gmail.com>
2024-12-07 11:01:17 -0300

    Clase19

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -4,11 +4,11 @@ from AppCoder.views import *
 
  
 urlpatterns = [
-    path('pagina inicio/', inicio),
-    path('curso/', curso),
-    path('profesores/', profesores),
-    path('estudiantes/', estudiantes),
-    path('entregables/', entregables),
+    path('pagina-inicio/', inicio, name="inicio"),
+    path('curso/', curso, name="curso"),
+    path('profesores/', profesores, name="profesores"),
+    path('estudiantes/', estudiantes,name="estudiantes"),
+    path('entregables/', entregables,name="entregables"),
  
-]
+] 
    
