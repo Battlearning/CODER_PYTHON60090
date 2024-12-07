8ca42bac471d0c87c16536a28222f69056c4fdd7
Jessica Battistino <batt.learning@gmail.com>
2024-12-07 13:07:15 -0300

    Clase20

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -6,11 +6,17 @@ class Curso(models.Model):
     nombre = models.CharField(max_length=100)
     camada = models.IntegerField()
 
+    def __str__(self):
+        return f"Nombre del curso:  {self.nombre} -- Número de Comisión: {self.camada}"
+
 
 class Estudiante(models.Model):
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30) 
     email = models.EmailField ()
+
+    def __str__(self):
+        return f"Alumno:  {self.nombre} {self.apellido}"
 
 
 class Profesor(models.Model):
@@ -18,11 +24,15 @@ class Profesor(models.Model):
     apellido = models.CharField(max_length=30) 
     email = models.EmailField ()
     profesion = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"Profesor:  {self.nombre} {self.apellido}, {self.profesion}"
     
      
-class Emtregable(models.Model):
+class Entregable(models.Model):
     nombre = models.CharField(max_length=100)
     fechadeentrega = models.DateField() 
     entregado = models.BooleanField()
 
-      
+    def __str__(self):
+        return f"Entrega:  {self.nombre}, estado:  {self.entregado}"
